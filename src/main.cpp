9247985683d057bca12f169fbb512d122bbd04cc
twobit <s8572327@gmail.com>
2017-02-07 21:08:37 +0100

    triangulate

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <cstdlib>
 #include <cstdio>
 #include <cmath>
+#include <algorithm>
 
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtx/transform.hpp>
@@ -10,11 +11,84 @@
 #include "eye.h"
 #include "editor.h"
 
+namespace {
+
+	inline bool is_oriented_cw(const glm::vec2& a, const glm::vec2& b, const glm::vec2& c) {
+		glm::vec2 ab = b - a;
+		glm::vec2 ac = c - a;
+		return ab.y * ac.x - ab.x * ac.y >= 0;
+	}
+	bool on_same_side(const glm::vec2& a, const glm::vec2& b, const glm::vec2& c, const glm::vec2& d) {
+		glm::vec2 p = d - c;
+		glm::vec2 ca = a - c;
+		glm::vec2 cb = b - c;
+		float l = p.x * ca.y - p.y * ca.x;
+		float m = p.x * cb.y - p.y * cb.x;
+		return l * m >= 0;
+	}
+	bool point_in_triangle(const glm::vec2& p, const glm::vec2& a, const glm::vec2& b, const glm::vec2& c) {
+		return on_same_side(p, a, b, c) && on_same_side(p, b, a, c) && on_same_side(p, c, a, b);
+	}
+	bool any_point_in_triangle(const std::vector<const glm::vec2*>& vertices, const glm::vec2& a, const glm::vec2& b, const glm::vec2& c) {
+		for (const glm::vec2* p : vertices) {
+			if ((p != &a) && (p != &b) && (p != &c) && point_in_triangle(*p, a, b, c)) return true;
+		}
+		return false;
+	}
+	bool is_ear(const glm::vec2& a, const glm::vec2& b, const glm::vec2& c, const std::vector<const glm::vec2*>& vertices) {
+		return is_oriented_cw(a, b, c) && !any_point_in_triangle(vertices, a, b, c);
+	}
+	template <class Func>
+	bool triangulate(const std::vector<glm::vec2>& poly, Func f) {
+		std::vector<int> prev_index(poly.size());
+		std::vector<int> next_index(poly.size());
+		for (int i = 0; i < (int) poly.size(); ++i) {
+			prev_index[i] = i - 1;
+			next_index[i] = i + 1;
+		}
+		prev_index[0] = poly.size() - 1;
+		next_index[poly.size() - 1] = 0;
+
+		std::vector<const glm::vec2*> concave;
+		for (int i = 0; i < (int) poly.size(); ++i) {
+			if (!is_oriented_cw(poly[prev_index[i]], poly[i], poly[next_index[i]])) {
+				concave.push_back(&poly[i]);
+			}
+		}
+
+		int left = poly.size();
+		int skipped = 0;
+		int current = 1;
+		int next, prev;
+		while (left > 3) {
+			prev = prev_index[current];
+			next = next_index[current];
+			const glm::vec2& a = poly[prev];
+			const glm::vec2& b = poly[current];
+			const glm::vec2& c = poly[next];
+			if (is_ear(a, b, c, concave)) {
+				f(a, b, c);
+				next_index[prev] = next;
+				prev_index[next] = prev;
+				auto it = std::find(concave.begin(), concave.end(), &b);
+				if (it != concave.end()) concave.erase(it);
+				--left;
+				skipped = 0;
+			}
+			else if (++skipped > left) {
+				printf("WOOT\n");
+				return false;
+			}
+			current = next;
+		}
+		next = next_index[current];
+		prev = prev_index[current];
+		f(poly[prev], poly[current], poly[next]);
 
 
-template <class Func>
-bool triangulate(const std::vector<glm::vec2>& poly, Func f) {
-	return true;
+		return true;
+	}
+
 }
 
 
